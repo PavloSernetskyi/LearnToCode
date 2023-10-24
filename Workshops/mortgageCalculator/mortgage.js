dde3ac212931032fe0136ee398c7782f1e8db8d3
Pavlo_Sernetskyi <pavlosernetskyi@yahoo.com>
2023-10-23 19:05:08 -0700

    mortgage calculator js code solved

--- Workshops/mortgageCalculator/mortgage.js
+++ Workshops/mortgageCalculator/mortgage.js
@@ -1,52 +1,30 @@
 "use strict"
  // mortgate M. calculator.
-let principal = 53000; // loan?
+let principal = 53000; // loan
 let interestRate = 0.07625;  // %7.625. // annual interest rate.
 let loanLength = 15;
 
 let monthlyPayment = 0;
 let totalNumOfPayments = 0;
 
-//calculation for monthy interest rate.
-let monthlyInterestRate = (principal * interestRate) / 12;
+//calculation for monthly interest rate. (annual interest rate / 12 months).
+let monthlyInterestRate = interestRate / 12;
 
-//n.
+//number of total payments.
 totalNumOfPayments = 12 * loanLength; // 15 years times 12 months.
-console.log("montly interest rate  " + monthlyInterestRate);
 
-//calculate monthly payment
-
+//calculate monthly payment using formula. M = P [ r(1 + r)^n ] / [ (1 + r)^n – 1]
 let numerator = monthlyInterestRate * Math.pow((1 + monthlyInterestRate), totalNumOfPayments);
 let denumerator = Math.pow((1 + monthlyInterestRate), totalNumOfPayments) - 1;
-console.log("Numberator " + numerator);
-console.log("Denumerator " + denumerator);
+
 monthlyPayment = principal * (numerator / denumerator);
 
+console.log(`Montly payment is $${monthlyPayment.toFixed(2)} and total interest paid is:`);
 
+//part two calculate total interest paid.
+// using formula Total Interest = (M x n) - P
+let totalInterest = 0;
 
-console.log(`Montly payment is ${monthlyPayment} and total interest paid is:`);
+totalInterest = (monthlyPayment * totalNumOfPayments) - principal;
 
-
-
-
-// // Use the mortgage formula to calculate the monthly payment
-// const monthlyPaymentUnrounded = principalLoanAmount *
-//     (monthlyInterestRateDecimal * Math.pow(1 + monthlyInterestRateDecimal, totalNumberOfPayments)) /
-//     (Math.pow(1 + monthlyInterestRateDecimal, totalNumberOfPayments) - 1);
-
-//     console.log(`The monthly payment is ${monthlyPaymentUnrounded}`);
-//     console.log(`The monthly payment is ${monthlyPaymentUnrounded}`);
-
-
-
-    // Use the mortgage formula to calculate the monthly payment
-//const monthlyPaymentUnrounded = principalLoanAmount * 
-//(monthlyInterestRateDecimal * Math.pow(1 + monthlyInterestRateDecimal, totalNumberOfPayments)) / 
-//(Math.pow(1 + monthlyInterestRateDecimal, totalNumberOfPayments) - 1);
-//M is the total monthly mortgage payment.
-//P is the principal loan amount.
-//r is your monthly interest rate. This is your annual interest rate divided by 12 (the number of months in a year).
-//n is your number of payments (the number of months you will be paying the loan).
-
-
-
+console.log(`Total interest is $${totalInterest.toFixed(2)}`);
