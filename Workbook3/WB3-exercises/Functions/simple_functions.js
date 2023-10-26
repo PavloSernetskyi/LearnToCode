196baacc4440b8f370317ff15d1425a0a614abd2
Pavlo_Sernetskyi <pavlosernetskyi@yahoo.com>
2023-10-26 08:50:52 -0700

    simple_functions
    simple_functions

---
+++ Workbook3/WB3-exercises/Functions/simple_functions.js
@@ -0,0 +1,26 @@
+"use strict"
+
+let myName ="Pavlo";
+let favoriteMovie ="Avengers";
+let musician = "Jimmy Hendrix";
+
+
+function favoriteThings () {
+    console.log(`My name is ${myName}`);
+    console.log(`My favorite movie is ${favoriteMovie}`);
+    console.log(`Favorite musician is ${musician}`);
+}
+
+favoriteThings();
+
+function whyImHere(){
+    console.log(`I have joined learn to code program to learn HTML, CSS and JavaScript!`);
+}
+
+whyImHere();
+
+function favoritePlace(){
+    console.log(`Favorite place is Italy`);
+}
+
+favoritePlace();
