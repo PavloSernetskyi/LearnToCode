3d50e2e884d28e7b0d411c00289dca26d2fff4c5
Pavlo_Sernetskyi <pavlosernetskyi@yahoo.com>
2023-10-26 11:32:04 -0700

    display user input as date in browser

---
+++ Workbook3/WB3-exercises/Dates/OffBy1Website/index.js
@@ -0,0 +1,24 @@
+"use strict"
+
+window.onload = init;
+
+function init(){
+    //grab button from HTML by ID to click on. 
+    //and call other function when clicking on button.
+    let button = document.getElementById('btn1');
+    button.onclick = displayDate;
+}
+
+function displayDate(){
+    // console.log("Hey");
+    // grab input date from user.
+    let dateInput = document.getElementById('userInp').value;
+
+    // display in 2 steps.
+    let message = (`The date is: ${dateInput}`);
+    document.getElementById('output').innerText = message;
+}
+
+function message(){
+    console.log("Hi");
+}
