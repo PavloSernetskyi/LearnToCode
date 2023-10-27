1d29b6307b590aa3b03d79c522c32edef9f04a74
Pavlo_Sernetskyi <pavlosernetskyi@yahoo.com>
2023-10-27 13:53:54 -0700

    advanced_parsing
    advanced_parsing

--- Workbook3/WB3-exercises/Functions/advanced_parsing.js
+++ Workbook3/WB3-exercises/Functions/advanced_parsing.js
@@ -16,15 +16,41 @@ let name3 = "Dana Lynn Wyatt";
 // Middle name: Lynn
 // Last name: Wyatt
 
-console.log("Name: " + name1);
-console.log("Only name: " + name1);
-console.log("Name: " + name2);
-console.log("First name: " + name2.substring(0,6));
-console.log("Last name: " + name2.substring(6,10));
-console.log("Name: " + name3);
-console.log("First name: " + name3.substring(0,4));
-console.log("Middle name: " + name3.substring(5,9));
-console.log("Last name: " + name3.substring(10,15));
+// console.log("Name: " + name1);
+// console.log("Only name: " + name1);
+// console.log("Name: " + name2);
+// console.log("First name: " + name2.substring(0,6));
+// console.log("Last name: " + name2.substring(6,10));
+// console.log("Name: " + name3);
+// console.log("First name: " + name3.substring(0,4));
+// console.log("Middle name: " + name3.substring(5,9));
+// console.log("Last name: " + name3.substring(10,15));
 
 
+//function...
+function parseAndDisplayName(_userName) {
+    // grab " " position in the string.
+    let space = _userName.indexOf(" ");
+    let space2 = _userName.lastIndexOf(" ");
 
+    //grab first name/ grab string until it hits " " - space char.
+    let firstName = _userName.slice(0, space);
+    //grab the middle name after " " - space char.
+    let middleName = _userName.slice(space + 1, space2);
+
+    // grab string after last " " - char.
+    let lastName = _userName.slice(space2 + 1);
+
+    console.log(`Name: ${_userName}`);
+    console.log(`First name: ${firstName}`);
+    console.log(`Middle name: ${middleName}`);
+    console.log(`Last name: ${lastName}`);
+}
+
+parseAndDisplayName("Brenda Kate Smith");
+// parseAndDisplayName("Ian Auston");
+// parseAndDisplayName("Siddalee Grace");
+
+parseAndDisplayName("Cher");
+parseAndDisplayName("Brenda Kaye");
+parseAndDisplayName("Dana Lynn Wyatt");
--- Workbook3/WB3-exercises/Functions/parsing.js
+++ Workbook3/WB3-exercises/Functions/parsing.js
@@ -22,13 +22,13 @@ let name2 = "John Cena";
  * @param {string} x - string argument for the function.
  * no return value. 
  */
-function parseAndDisplayName(x){
+function parseAndDisplayName(x) {
     //parsing.
-let firstName = x.substring(0,6);
-let lastName = x.substring(6,10);
+    let firstName = x.substring(0, 6);
+    let lastName = x.substring(6, 10);
 
-console.log(`First name: ${firstName}`);
-console.log(`Last name: ${lastName}`);
+    console.log(`First name: ${firstName}`);
+    console.log(`Last name: ${lastName}`);
 }
 
 parseAndDisplayName(name);
@@ -37,5 +37,43 @@ parseAndDisplayName(name2);
 
 let myName = "Pavlo Sernetskyi";
 
-console.log("My first name is: " + myName.slice(0,5));
-console.log("My last name is: " + myName.slice(5,16));
+// grab " " position in the string.
+let space = myName.indexOf(" ");
+
+//slices/works with strings of any size of first name.
+console.log("My first name is: " + myName.slice(0, space));
+console.log("My last name is: " + myName.slice(space + 1));
+
+// slices only hardcoded positons
+// console.log("My first name is: " + myName.slice(0,5));
+// console.log("My last name is: " + myName.slice(5,16));
+
+
+// put my code into function to work with string of diferent sizes. using " " as breaking point for spliting my string. 
+// using index.Of(" ") to grab position.
+
+let myName2 = "Peter Sernetskyi";
+
+console.log();
+console.log();
+
+
+
+//function...
+function parseAndDisplayName(_userName) {
+    // grab " " position in the string.
+    let space2 = _userName.indexOf(" ");
+
+    //grab first name/ grab string until it hits " " - space char.
+    let firstName = _userName.slice(0, space2);
+    //grab the rest of the string after " " - space char.
+    let lastName = _userName.slice(space2 + 1);
+
+    console.log(`Name: ${_userName}`);
+    console.log(`First name: ${firstName}`);
+    console.log(`Last name: ${lastName}`);
+}
+
+parseAndDisplayName("Brenda Kaye");
+parseAndDisplayName("Ian Auston");
+parseAndDisplayName("Siddalee Grace");
