6489464b3f87fde39dddcc4134a68d3927a4a348
Pavlo_Sernetskyi <pavlosernetskyi@yahoo.com>
2023-10-30 13:27:25 -0700

    dates, anonymous function

--- Workbook3/WB3-exercises/Dates/DateScripts/date_difference.js
+++ Workbook3/WB3-exercises/Dates/DateScripts/date_difference.js
@@ -1,2 +1,13 @@
 "use strict"
 
+let startDate = new Date("July 11, 2022");
+let endDate = new Date("November 11, 2022");
+
+//dai in milisecond = 24 * 60min * 60 sec * 1000ms.
+let dayInMs = 24 * 60 * 60 * 1000;
+
+let msBetweenTwoDates = endDate.getTime() - startDate.getTime();
+
+let daysBetweenTwoDates = msBetweenTwoDates / dayInMs;
+
+console.log("There are " + Math.round(daysBetweenTwoDates) + " days between two dates");
