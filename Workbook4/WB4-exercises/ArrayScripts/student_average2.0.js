c6be171eaf44d062ce832ce185cc6ebc31110d82
Pavlo_Sernetskyi <pavlosernetskyi@yahoo.com>
2023-11-03 12:51:26 -0700

    student.avg scores 2.0

--- Workbook4/WB4-exercises/ArrayScripts/student_average2.0.js
+++ Workbook4/WB4-exercises/ArrayScripts/student_average2.0.js
@@ -10,12 +10,19 @@ let students = [
 ];
 
 for (let index = 0; index < students.length; index++) {
-    console.log(`Students: ${students[index].name}`);
-    // usinginner loop to loop through array of scores to find average of each student.
-    let averageOfEachStudent = 0;
-    for (let k = 0; k < students[index].scores; k++) {
-        averageOfEachStudent += students[index].scores;
-        console.log(`${students[index].scores}`);
+    console.log(`Student name: ${students[index].name}`);
+
+    // usinginner loop to loop through array of scores to 1) sum up each array and then 2) find average of each student's scores.
+    let sumOfEachArrayOfScores
+        = 0;
+    for (let j = 0; j < students[index].scores.length; j++) {
+        sumOfEachArrayOfScores
+            = sumOfEachArrayOfScores
+            + students[index].scores[j];
     }
-    
+    console.log(`Scores: ${students[index].scores}`);
+    console.log(`Sum of each array is ${sumOfEachArrayOfScores
+        }`);
+    let eachStudentAvg = sumOfEachArrayOfScores / students[index].scores.length;
+    console.log("Each student average: " + eachStudentAvg.toFixed(2) +"\n");
 }
