fa550a6ae43ed41dd3d351f2e8ffb97376b8e4ae
Pavlo_Sernetskyi <pavlosernetskyi@yahoo.com>
2023-11-09 16:05:57 -0800

    save point 1

--- Capstone2 Template/scripts/app.js
+++ Capstone2 Template/scripts/app.js
@@ -1,4 +1,50 @@
 "use strict";
 
-//Declaring variables
-console.log(locationsArray.length);
+window.onload = () => {
+  //
+  const states = document.getElementById("states");
+  const parktype = document.getElementById("parktype");
+  addLocationsToDropdown();
+  addNationalParksToDropdown();
+};
+
+function addLocationsToDropdown() {
+  // Get the dropdown element by its ID
+
+  // Use forEach to iterate over the array
+  locationsArray.forEach(function (location) {
+    // Create a new option element
+    let option = document.createElement("option");
+
+    // Set the value and text content for the new option
+    option.value = location;
+    option.textContent = location;
+
+    // Add the new option to the dropdown
+    states.appendChild(option);
+  });
+}
+
+function addNationalParksToDropdown() {
+  // Get the dropdown element by its ID
+
+  // Use forEach to iterate over the array
+  nationalParksArray.forEach(function (park) {
+    // Create a new option element
+    let option = document.createElement("option");
+
+    // Set the value and text content for the new option
+    option.value = park.LocationName;
+    option.textContent = park.LocationName;
+
+    // Add the new option to the dropdown
+    parktype.appendChild(option);
+  });
+}
+// grab the value of a selected item from the dropdown.
+function searchLocation() {
+  const selectedValue = states.value;
+  alert(selectedValue);
+}
+
+// Declaring variables
