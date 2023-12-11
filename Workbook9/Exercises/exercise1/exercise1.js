3a2a9c6776fa0adcbace03502b4663687e15b2d4
Pavlo_Sernetskyi <pavlosernetskyi@yahoo.com>
2023-12-11 15:44:43 -0800

    OOP exercise 1,2,3 done.
    created class with constructor and 3 methods, getFullName, promotion and getIntro that will create new employees and update theur salary, position. etc. This code demonstrates OOP Classes , reusability of the functions by creating multiple employees objects.

--- Workbook9/Exercises/exercise1/exercise1.js
+++ Workbook9/Exercises/exercise1/exercise1.js
@@ -10,20 +10,64 @@ class Employee {
     }
 
     //getFullName.
-    fullName() {
+    getFullName() {
         let fullEmpName = this.firstName + " " + this.lastName;
         return fullEmpName;
     }
-}
 
-// let employee1 = new Employee(
-//     1, "Ian", "Auston", "Graphic Artist", 42.50);
+    // promote method. increase pay rate and position.
+    promote(newJobTitle, newPayRate) {
+        this.jobTitle = newJobTitle;
+        this.payRate = newPayRate;
 
+    } //end of promote func.
 
+    // getIntro method.
+    getIntro() {
+        let intro =
+            "Hi! I'm " + this.getFullName() + " and I am a " +
+            this.jobTitle;
+        return intro;
+    }
 
+}//end of class Employee
+
+// test exercise 1.
+//emp 1.
 let employee1 = new Employee(
     1, "Ian", "Auston", "Graphic Artist", 42.50);
-console.log(`Employee ${employee1.fullName()} created`);
+console.log(`Employee ${employee1.getFullName()} created`);
 console.log(`Job title is ${employee1.jobTitle}`);
 console.log(`Pay rate is $${employee1.payRate}`);
+console.log();
 
+//emp 2.
+let employee2 = new Employee(
+    1, "Paul", "Stevens", "Network Engineer", 55.50);
+console.log(`Employee ${employee2.getFullName()} created`);
+console.log(`Job title is ${employee2.jobTitle}`);
+console.log(`Pay rate is $${employee2.payRate}`);
+console.log();
+console.log();
+
+// exercise 2 test.
+console.log("Promotions...");
+console.log("Employee 1 promotion.");
+
+// emp1 promotion
+employee1.promote("Sr. Graphic Artist", 46.75);
+console.log(`Job title is ${employee1.jobTitle}`);
+console.log(`Pay rate is $${employee1.payRate}`);
+console.log();
+
+// emp2 promotion.
+console.log("Employee 2 promotion.");
+employee2.promote("Sr. Network Architect", 60.75);
+console.log(`Job title is ${employee2.jobTitle}`);
+console.log(`Pay rate is $${employee2.payRate}`);
+
+// exercise 3 test. display intro.
+let intro = employee1.getIntro();
+console.log(intro);
+let intro2 = employee2.getIntro();
+console.log(intro2);
